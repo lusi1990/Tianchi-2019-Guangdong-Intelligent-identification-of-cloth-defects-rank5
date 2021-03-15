390fc9af14f89b16b00fdefbae33bb52000d605b
Jack <lusi2114@gmail.com>
2021-03-15 15:25:52 +0800

    优化 复制图片， 减少图片使用量

--- train/copy_detect_images.py
+++ train/copy_detect_images.py
@@ -17,11 +17,14 @@ def main():
         print('查看帮助: python3 copy_detect_images.py -h')
         return
     images = list()
+    template_dict = dict()
     for root_dir, dirs, files in os.walk(args.data_home):
         for file_name in files:
             if not file_name.endswith('jpg'):
                 continue
             if file_name.startswith('template_'):
+                temp_path = os.path.join(root_dir, file_name)
+                template_dict[os.path.split(root_dir)[-1]] = temp_path
                 continue
             if 'normal' in root_dir:
                 continue
@@ -29,9 +32,16 @@ def main():
             images.append(image_path)
             # print(root_dir, dirs, files)
     print(len(images))
+    # 复制检测图片
     for image_path in images:
         print(f'copy {image_path}')
         shutil.copy(image_path, '../data/fabric/defect_Images')
+    # 复制模板图片
+    for name, template_path in template_dict.items():
+        template_home = f'../data/fabric/template_Images/{name}'
+        if not os.path.isdir(template_home):
+            os.makedirs(template_home)
+        shutil.copy(template_path, f'../data/fabric/template_Images/{name}')
 
 
 if __name__ == '__main__':
--- train/train.sh
+++ train/train.sh
@@ -12,20 +12,18 @@ mkdir -p ../data/pretrained
 
 python3 copy_detect_images.py -d $DATA_HOME
 
-cp -r $DATA_HOME/guangdong1_round2_train_part1_20190924/defect/* ../data/fabric/template_Images/ &&
-  cp -r $DATA_HOME/guangdong1_round2_train_part2_20190924/defect/* ../data/fabric/template_Images/ &&
-  cp -r $DATA_HOME/guangdong1_round2_train_part3_20190924/defect/* ../data/fabric/template_Images/ &&
-  cp -r $DATA_HOME/guangdong1_round2_train2_20191004_images/defect/* ../data/fabric/template_Images/
-
 cp $DATA_HOME/guangdong1_round2_train_part1_20190924/Annotations/anno_train.json ../data/fabric/Annotations/anno_train_20190925.json
 cp $DATA_HOME/guangdong1_round2_train2_20191004_Annotations/Annotations/anno_train.json ../data/fabric/Annotations/anno_train_20191008.json
 python merage_data_json.py
+
 python guangdong_round2.py
 echo "guangdong_round2.py over"
 python guangdong_round2_100.py
 echo "guangdong_round2_100.py over"
+
 wget https://open-mmlab.oss-cn-beijing.aliyuncs.com/mmdetection/models/cascade_rcnn_r50_fpn_20e_20181123-db483a09.pth -O ../data/pretrained/cascade_rcnn_r50_fpn_20e_20181123-db483a09.pth
 python transorfarm_concatenate_model.py
+
 CUDA_VISIBLE_DEVICES=0 ./dist_train.sh ../config/fabric_defect/cascade_rcnn_r50_fpn_70e.py 1
 CUDA_VISIBLE_DEVICES=0 ./dist_train.sh ../config/fabric_defect/cascade_rcnn_r50_fpn_400.py 1
 python publish_model.py ../data/work_dirs/cascade_rcnn_r50_fpn_70e/latest.pth ../data/work_dirs/cascade_rcnn_r50_fpn_70e/latest-submit.pth
