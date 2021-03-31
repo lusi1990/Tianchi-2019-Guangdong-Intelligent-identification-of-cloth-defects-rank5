885a29dea7db7aadb19787baa3e1f6aff371891a
Jack <lusi2114@gmail.com>
2021-03-31 15:08:47 +0800

    add transorfarm

---
+++ train/transorfarm_concatenate_model_data10.py
@@ -0,0 +1,32 @@
+"""
+for cascade rcnn
+cascadeRCNN是由一系列的检测模型组成，每个检测模型都基于不同IOU阈值的正负样本训练得到，前一个检测模型的输出作为后一个检测模型的输入，
+因此是stagebystage的训练方式，而且越往后的检测模型，其界定正负样本的IOU阈值是不断上升的。
+"""
+import torch
+import os
+from torch.nn import init
+import numpy as np
+
+model_name = "../data/pretrained/cascade_rcnn_r50_fpn_20e_20181123-db483a09.pth"
+
+model_coco = torch.load(model_name)
+
+# weight
+model_coco["state_dict"]["backbone.conv1.weight"] = torch.cat([model_coco["state_dict"]["backbone.conv1.weight"]] * 2,
+                                                              dim=1)
+# todo  需要添加如下代码才不会报错，为什么
+num_class = 9
+model_coco['state_dict']['bbox_head.0.fc_cls.weight'].resize_(num_class, 1024)
+model_coco['state_dict']['bbox_head.1.fc_cls.weight'].resize_(num_class, 1024)
+model_coco['state_dict']['bbox_head.2.fc_cls.weight'].resize_(num_class, 1024)
+model_coco['state_dict']['bbox_head.0.fc_cls.bias'].resize_(num_class)
+model_coco['state_dict']['bbox_head.1.fc_cls.bias'].resize_(num_class)
+model_coco['state_dict']['bbox_head.2.fc_cls.bias'].resize_(num_class)
+model_coco['state_dict']['bbox_head.0.fc_reg.weight'].resize_(4, 1024)
+
+print(model_coco["state_dict"]["backbone.conv1.weight"].shape)
+
+# save new model
+torch.save(model_coco,
+           "../data/pretrained/concatenate_coco_pretrained_" + os.path.basename(model_name).split('.')[0] + ".pth")
